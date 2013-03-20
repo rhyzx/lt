d5df688d0ad6b83c454078f631c93bda9066bfa4
U0 <rhyzix@gmail.com>
2013-03-20 11:41:47 +0800

    opt

--- lt.js
+++ lt.js
@@ -13,20 +13,18 @@
     // core
     function compile(source) {
         var vars = "_DATA = _DATA || {}; var "
-        var body = "_S = '" +source
-            .replace(re.backSlash, "\\\\")  // escape \
-            .replace(re.quote, "\\'")       // escape '
+        var body = "_TMP = '" +source
+            .replace(re.backSlash, "\\\\")      // escape \
+            .replace(re.quote, "\\'")           // escape '
             .replace(re.va, function (a, v) {
-                vars += v +"=_DATA." +v +", " // pre-define variable
-                return "' +" +v +" +'"      // print variables
+                vars += v +"=_DATA." +v +", "   // pre-define variable
+                return "' +" +v +" +'"          // print variables
             })
-            .replace(re.stmt, "'; $1 _S+='") // statements
-            .replace(re.cr, "\\n")          // escape cr
-        +"'; return _S"
-        var template =  new Function('_DATA',
-            vars +body
-        )
-        return template.render = template   // render api
+            .replace(re.stmt, "'; $1 _TMP+='")  // statements
+            .replace(re.cr, "\\n")              // escape cr
+            +"'; return _TMP"
+        var template =  new Function('_DATA', vars +body)
+        return template.render = template       // render api
     }
 
     // compile api
