944b2e9a569d4c88a8ad0ebd7085971b37bd8cbf
U0 <rhyzix@gmail.com>
2013-03-21 20:34:46 +0800

    opt

--- lt.js
+++ lt.js
@@ -1,18 +1,33 @@
 !(function (root, undefined) {
     "use strict" 
 
+    // help function
+    var _isArray = Array.isArray || function(obj) {
+        return Object.prototype.toString.call(obj) === '[object Array]'
+    }
+
     // escape HTML chars http://www.w3.org/TR/html4/charset.html#h-5.3.2
     var escapes = { '<': '&lt;', '>': '&gt;', '&': '&amp;', '"': '&quot;' }
-    function get(val, escape) {
-        if (typeof val === 'undefined') return ''
-        return !escape ? val : (val +'').replace(/[<>&"]/g, function (char) {
-            return escapes[char]
-        })
+    var hook = { // this hook
+        print : function (val, escape) { // get string and escape
+            if (typeof val === 'undefined') return ''
+            return !escape ? val : (val +'').replace(/[<>&"]/g, function (char) {
+                return escapes[char]
+            })
+        }
+      , each : function (ctx, parent, reverse, iterator) {
+            var arr = ctx ? _isArray(ctx) ? ctx : [ctx] : []
+            arr = reverse ? arr.length ? [] : [parent] : arr
+            if (arr.forEach) return arr.forEach(iterator, this)
+            for (var i=0, len=arr.length; i<len; i++) {
+                iterator.call(this, arr[i], i, arr)
+            }
+        } 
     }
 
     // core
     function compile(source) {
-        var body = "ctx = ctx || {}; result = '" +source
+        var body = "ctx = ctx || {}; var result = '" +source
             .replace(/\\/g, "\\\\") // escape \
             .replace(/'/g, "\\'") // escape '
             .replace(/\{\{(.+?)\}\}/g, function (a, scope) { // block
@@ -21,31 +36,26 @@
                 case '^':   // if not
                     reverse = true
                 case '#':   // if/each/TODO helper
-                    return "';(function (ctx, parent) {" // block context
-                            + "var arr = ctx "
-                            + "? Object.prototype.toString.call(ctx) === '[object Array]' "
-                            + (reverse ? "? [] : [] : [parent]" : "? ctx : [ctx] : []")
-                            + "; for (var i=0, len=arr.length; i<len; i++)"
-                            + "{ ctx = arr[i]; result+='"
+                    return "'; this.each(ctx." +scope.slice(1) +", ctx"
+                            + ", " +reverse +", function (ctx) { result += '"
                 case '/':   // close block
-                    return "'; }}).call(this, ctx." +scope.slice(1) +", ctx); result+= '"
+                    return "'; }); result += '"
                 case '!':   // comments
                     return ""
                 case '&':   // print unescape
                     escape = false
                 default :   // print escape
-                    return "' +this(ctx" +(scope === 'this' ? '' : '.' +scope) +", " +escape +") +'"
+                    return "' +this.print(ctx"
+                            + (scope === 'this' ? '' : '.' +scope)
+                            + ", " +escape +") +'"
                 }
             })
             .replace(/\n/g, "\\n") // escape cr
             +"'; return result"
 
         var compiled = new Function('ctx', body)
-          , template = function (data) {
-            return compiled.call(get, data)
-        }
-        template.toString = function () {
-            return compiled.toString()
+        var template = function (data) {
+            return compiled.call(hook, data)
         }
         return template.render = template // render api
     }
