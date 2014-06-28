f86821240a08278be700bdfee39a9afd2388c4da
U0 <rhyzix@gmail.com>
2014-06-28 22:32:45 +0800

    indent with 2 spaces and clean code

--- lt.js
+++ lt.js
@@ -1,85 +1,79 @@
-/*!
- * LT - Little Template engine of {{mustache}}
- * https://github.com/rhyzx/lt
- */
-!(function (root, undefined) {
-    var _isArray = Array.isArray || function(obj) {
-        return Object.prototype.toString.call(obj) === '[object Array]'
-    }
+!function () {
 
-    // print value
-    function print(value, escape) {
-        return typeof value === 'undefined'
-             ? '' // placeholder
-             : ( escape && /[&"<>]/.test(value += '') )
-             // escape HTML chars http://www.w3.org/TR/html4/charset.html#h-5.3.2
-             ? value.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-             : value
-    }
+var _isArray = Array.isArray || function(obj) {
+  return Object.prototype.toString.call(obj) === '[object Array]'
+}
 
-    // get defined value from context stack
-    function get(scope, depth) {
-        scope = scope
-        .replace(/\s/g, '') // clear space
-        .replace(/^(\.\.\/)+/, function (all, one) {
-            depth -= all.length / one.length // parent path
-            return ''
-        })
-        if (depth < 0) return "undefined"
-        if (scope === '.') return "s" +depth // this
-        var first = scope.match(/^[^.]+/)[0]  // nest path support, extract first scope for context finding
-        var code = ''
-        while (depth > 0) code += "typeof s" +depth +"." +first +" !== 'undefined' ? s" +depth-- +"." +scope +" : "
-        return code +"s0." +scope
-    }
+// print value
+function print(value, escape) {
+  return typeof value === 'undefined'
+  ? '' // placeholder
+  : ( escape && /[&"<>]/.test(value += '') )
+  // escape HTML chars http://www.w3.org/TR/html4/charset.html#h-5.3.2
+  ? value.replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+  : value
+}
 
-    // core
-    function compile(source) {
-        var inverted = 0, depth = 0 // context stack depth
-        var compiled = new Function("s0", "print", "_isArray", "var tmp, out = '" +source
-        .replace(/\\/g, "\\\\") // escape \
-        .replace(/'/g, "\\'")   // escape '
-        .replace(/\{\{([\^#/!&]?)([^{\n]+?)\}\}/g, function (a, flag, scope) { // block
-            switch (flag) {
-            case '^':   // if not
-                inverted++
-                return "'; tmp = " +get(scope, depth)
-                     + " ; if (!tmp || (_isArray(tmp) && tmp.length === 0)) { out += '"
-            case '#':   // if/each/TODO lambdas/TODO helper
-                return "'; tmp = " +get(scope, depth)
-                     + " ; var list" +depth +" = tmp ? _isArray(tmp) ? tmp : [tmp] : []"
-                     + " ; for (var i" +depth +"=0, len" +depth +"=list" +depth +".length"
-                     + " ;      i" +depth +"<len" +depth +"; i" +depth +"++)"
-                     + " { var s" +(depth+1) +" = list" +depth +"[i" +depth++ +"]; out += '"
-            case '/':   // close
-                inverted > 0 ? inverted-- : depth--
-                return "'} out += '"
-            case '!':   // comments
-                return ""
-            //case '>':   // TODO partials
-            case '&':   // print noescape
-                return "' +print(" +get(scope, depth) +", false) +'"
-            default :   // print escape
-                return "' +print(" +get(scope, depth) +", true) +'"
-            }
-        })
-        .replace(/\n/g, "\\n") // escape CR
-        .replace(/\r/g, "\\r") // escape LF
-        +"'; return out")
+// get defined value from context stack
+function get(scope, depth) {
+  scope = scope
+  .replace(/\s/g, '') // clear space
+  .replace(/^(\.\.\/)+/, function (all, one) {
+    depth -= all.length / one.length // parent path
+    return ''
+  })
+  if (depth < 0) return "undefined"
+  if (scope === '.') return "s" +depth // this
+  var first = scope.match(/^[^.]+/)[0]  // nest path support, extract first scope for context finding
+  var code = ''
+  while (depth > 0) code += "typeof s" +depth +"." +first +" !== 'undefined' ? s" +depth-- +"." +scope +" : "
+  return code +"s0." +scope
+}
 
-        var template = function (data) {
-            return compiled(data, print, _isArray)
-        }
-        return template.render = template // render api
+// core
+function compile(source) {
+  var inverted = 0, depth = 0 // context stack depth
+  var compiled = new Function("s0", "print", "_isArray", "var tmp, out = '" +source
+  .replace(/\\/g, "\\\\") // escape \
+  .replace(/'/g, "\\'")   // escape '
+  .replace(/\{\{([\^#/!&]?)([^{\n]+?)\}\}/g, function (a, flag, scope) { // block
+    switch (flag) {
+    case '^':   // if not
+      inverted++
+      return "'; tmp = " +get(scope, depth)
+           + " ; if (!tmp || (_isArray(tmp) && tmp.length === 0)) { out += '"
+    case '#':   // if/each/TODO lambdas/TODO helper
+      return "'; tmp = " +get(scope, depth)
+           + " ; var list" +depth +" = tmp ? _isArray(tmp) ? tmp : [tmp] : []"
+           + " ; for (var i" +depth +"=0, len" +depth +"=list" +depth +".length"
+           + " ;      i" +depth +"<len" +depth +"; i" +depth +"++)"
+           + " { var s" +(depth+1) +" = list" +depth +"[i" +depth++ +"]; out += '"
+    case '/':   // close
+      inverted > 0 ? inverted-- : depth--
+      return "'} out += '"
+    case '!':   // comments
+      return ""
+    //case '>':   // TODO partials
+    case '&':   // print noescape
+      return "' +print(" +get(scope, depth) +", false) +'"
+    default :   // print escape
+      return "' +print(" +get(scope, depth) +", true) +'"
     }
-    compile.compile = compile // compile api
+  })
+  .replace(/\n/g, "\\n") // escape CR
+  .replace(/\r/g, "\\r") // escape LF
+  +"'; return out")
 
-    // exports
-    if (typeof module !== 'undefined' && module.exports) {
-        module.exports = compile // CommonJS
-    } else if (typeof define === 'function' && define.amd) {
-        define('lt', compile)   // AMD
-    } else {
-        root['lt'] = compile    // <script>
-    }
-})(this)
+  var render = function (data) {
+    return compiled(data, print, _isArray)
+  }
+  return render.render = render // render api
+}
+compile.compile = compile // compile api
+
+// export
+if (typeof define === 'function' && define.amd) define(function () { return compile })
+else if (typeof module === 'object' && module.exports) module.exports = compile
+else this.lt = compile
+
+}()
--- test/test.js
+++ test/test.js
@@ -1,266 +1,266 @@
 var assert = require('assert')
   , lt = require('..')
-    
+  
 describe('basic', function () {
-    var source  = 'this is {{name}}'
-      , data    = { name: 'foo' }
+  var source  = 'this is {{name}}'
+    , data    = { name: 'foo' }
 
-      , template
-      , result
+    , template
+    , result
 
-    it('should compile success', function () {
-        template = lt.compile(source)
-    })
+  it('should compile success', function () {
+    template = lt.compile(source)
+  })
 
-    it('should render success', function () {
-        result = template.render(data)
-    })
+  it('should render success', function () {
+    result = template.render(data)
+  })
 
-    it('should equal', function () {
-        assert.equal(result, 'this is foo')
-    })
+  it('should equal', function () {
+    assert.equal(result, 'this is foo')
+  })
 })
 
 describe('variable', function () {
-    it('should print true/false', function () {
-        var source = (function () {/*
-                {{a}}
-                {{b}}
-            */}).toString().slice(16, -4)
-        var data = { a: true, b: false }
+  it('should print true/false', function () {
+    var source = (function () {/*
+        {{a}}
+        {{b}}
+      */}).toString().slice(16, -4)
+    var data = { a: true, b: false }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('true'), -1)
-        assert.notStrictEqual(result.search('false'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('true'), -1)
+    assert.notStrictEqual(result.search('false'), -1)
+  })
 
-    it('should not print undefined value', function () {
-        var source = (function () {/*
-                {{undef}}
-                {{none}}
-            */}).toString().slice(16, -4)
-        var data = { undef: undefined}
+  it('should not print undefined value', function () {
+    var source = (function () {/*
+        {{undef}}
+        {{none}}
+      */}).toString().slice(16, -4)
+    var data = { undef: undefined}
 
-        var result = lt.compile(source).render(data)
-          
-        assert.strictEqual(result.replace(/\s/g, ''), '')
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.strictEqual(result.replace(/\s/g, ''), '')
+  })
 
-    it('should escape html chars', function () {
-         var source = (function () {/*
-                {{content}}
-            */}).toString().slice(16, -4)
-        var data = { content: '<html class="nice"> &x'}
+  it('should escape html chars', function () {
+     var source = (function () {/*
+        {{content}}
+      */}).toString().slice(16, -4)
+    var data = { content: '<html class="nice"> &x'}
 
-        var result = lt.compile(source).render(data)
-          
-        assert.strictEqual(result.search('<'), -1)
-        assert.strictEqual(result.search('>'), -1)
-        assert.strictEqual(result.search('"'), -1)
-        assert.strictEqual(result.search('&x'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.strictEqual(result.search('<'), -1)
+    assert.strictEqual(result.search('>'), -1)
+    assert.strictEqual(result.search('"'), -1)
+    assert.strictEqual(result.search('&x'), -1)
+  })
 
-    it('should not escape html chars', function () {
-         var source = (function () {/*
-                {{&content}}
-            */}).toString().slice(16, -4)
-        var data = { content: '<html class="nice"> &x'}
+  it('should not escape html chars', function () {
+     var source = (function () {/*
+        {{&content}}
+      */}).toString().slice(16, -4)
+    var data = { content: '<html class="nice"> &x'}
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('<'), -1)
-        assert.notStrictEqual(result.search('>'), -1)
-        assert.notStrictEqual(result.search('"'), -1)
-        assert.notStrictEqual(result.search('&x'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('<'), -1)
+    assert.notStrictEqual(result.search('>'), -1)
+    assert.notStrictEqual(result.search('"'), -1)
+    assert.notStrictEqual(result.search('&x'), -1)
+  })
 })
 
 describe('section', function () {
-    it('should use Non-False value', function () {
-        var source = (function () {/*
-            {{#val}}
-                here
-            {{/val}}
-            */}).toString().slice(16, -4)
-        var data = { val: true }
+  it('should use Non-False value', function () {
+    var source = (function () {/*
+      {{#val}}
+        here
+      {{/val}}
+      */}).toString().slice(16, -4)
+    var data = { val: true }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('here'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('here'), -1)
+  })
 
-    it('should use Object', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{name}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: {name: 'here'} }
+  it('should use Object', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{name}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: {name: 'here'} }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('here'), -1)
-    })
-    
-    it('should iterate every item in list', function () {
-        var source = (function () {/*
-            {{#list}}
-                {{a}}
-                {{b}}
-            {{/list}}
-            */}).toString().slice(16, -4)
-        var data = { list: [{a: 'foo'}, {b: 'bar'}] }
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('here'), -1)
+  })
+  
+  it('should iterate every item in list', function () {
+    var source = (function () {/*
+      {{#list}}
+        {{a}}
+        {{b}}
+      {{/list}}
+      */}).toString().slice(16, -4)
+    var data = { list: [{a: 'foo'}, {b: 'bar'}] }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('foo'), -1)
-        assert.notStrictEqual(result.search('bar'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('foo'), -1)
+    assert.notStrictEqual(result.search('bar'), -1)
+  })
 
-    it('should not iterate empty list', function () {
-        var source = (function () {/*
-            {{#list}}
-                here
-            {{/list}}
-            */}).toString().slice(16, -4)
-        var data = { list: [] }
+  it('should not iterate empty list', function () {
+    var source = (function () {/*
+      {{#list}}
+        here
+      {{/list}}
+      */}).toString().slice(16, -4)
+    var data = { list: [] }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.strictEqual(result.search('here'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.strictEqual(result.search('here'), -1)
+  })
 
-    it('should iterate nested list correctly', function () {
-        var source = (function () {/*
-            {{#list}}
-                {{#list}}
-                    {{v}}
-                {{/list}}
-            {{/list}}
-            */}).toString().slice(16, -4)
-        var data = { list: [
-            { list: [{v:1}, {v:2}]}
-          , { list: [{v:3}, {v:4}]}
-        ]}
+  it('should iterate nested list correctly', function () {
+    var source = (function () {/*
+      {{#list}}
+        {{#list}}
+          {{v}}
+        {{/list}}
+      {{/list}}
+      */}).toString().slice(16, -4)
+    var data = { list: [
+      { list: [{v:1}, {v:2}]}
+      , { list: [{v:3}, {v:4}]}
+    ]}
 
-        var result = lt.compile(source).render(data)
-        assert.notStrictEqual(result.search('4'), -1)
-    })
+    var result = lt.compile(source).render(data)
+    assert.notStrictEqual(result.search('4'), -1)
+  })
 })
 
 describe('inverted section', function () {
-    it('should use False', function () {
-        var source = (function () {/*
-            {{^val}}
-                {{a}}
-            {{/val}}
-            */}).toString().slice(16, -4)
-        var data = { val: false, a: 'yes' }
+  it('should use False', function () {
+    var source = (function () {/*
+      {{^val}}
+        {{a}}
+      {{/val}}
+      */}).toString().slice(16, -4)
+    var data = { val: false, a: 'yes' }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('yes'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('yes'), -1)
+  })
 
-    it('should not iterate list', function () {
-        var source = (function () {/*
-            {{^list}}
-                {{a}}
-                {{b}}
-            {{/list}}
-            */}).toString().slice(16, -4)
-        var data = { list: [{a: 'foo'}, {b: 'bar'}] }
+  it('should not iterate list', function () {
+    var source = (function () {/*
+      {{^list}}
+        {{a}}
+        {{b}}
+      {{/list}}
+      */}).toString().slice(16, -4)
+    var data = { list: [{a: 'foo'}, {b: 'bar'}] }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.strictEqual(result.search('foo'), -1)
-        assert.strictEqual(result.search('bar'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.strictEqual(result.search('foo'), -1)
+    assert.strictEqual(result.search('bar'), -1)
+  })
 })
 
 
 describe('scope inherits', function () {
-    it('should print scope of parent context if current not exist', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{name}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: {}, name: 'parent' }
+  it('should print scope of parent context if current not exist', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{name}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: {}, name: 'parent' }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('parent'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('parent'), -1)
+  })
 
-    it('should scope the defined context', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{name.val}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: { name: {} }, name: {val:'parent'} }
+  it('should scope the defined context', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{name.val}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: { name: {} }, name: {val:'parent'} }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.strictEqual(result.search('parent'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.strictEqual(result.search('parent'), -1)
+  })
 })
 
 describe('this context', function () {
 
-    it('should print object', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{.}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: {} }
+  it('should print object', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{.}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: {} }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('Object'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('Object'), -1)
+  })
 
-    it('should print list item', function () {
-        var source = (function () {/*
-            {{#list}}
-                {{.}}
-            {{/list}}
-            */}).toString().slice(16, -4)
-        var data = { list: [1, 2, 3, 4] }
+  it('should print list item', function () {
+    var source = (function () {/*
+      {{#list}}
+        {{.}}
+      {{/list}}
+      */}).toString().slice(16, -4)
+    var data = { list: [1, 2, 3, 4] }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('4'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('4'), -1)
+  })
 })
 
 describe('parent path', function (done) {
-    it('should print parent object', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{../.}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: {} }
+  it('should print parent object', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{../.}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: {} }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('Object'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('Object'), -1)
+  })
 
-    it('should print parent scope', function () {
-        var source = (function () {/*
-            {{#obj}}
-                {{../name}}
-            {{/obj}}
-            */}).toString().slice(16, -4)
-        var data = { obj: {name: 'child'}, name: 'parent' }
+  it('should print parent scope', function () {
+    var source = (function () {/*
+      {{#obj}}
+        {{../name}}
+      {{/obj}}
+      */}).toString().slice(16, -4)
+    var data = { obj: {name: 'child'}, name: 'parent' }
 
-        var result = lt.compile(source).render(data)
-          
-        assert.notStrictEqual(result.search('parent'), -1)
-    })
+    var result = lt.compile(source).render(data)
+      
+    assert.notStrictEqual(result.search('parent'), -1)
+  })
 })
